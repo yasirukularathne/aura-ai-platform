c4d8e9f808cbcaadcd66ae2e03bf8ae49eb8a149
yasirukularathne <yasirukularathne1234@gmail.com>
2026-09-24 06:34:35 +0530

    refactor: externalize Qdrant configuration

--- backend/app/rag/retrieval/retriever.py
+++ backend/app/rag/retrieval/retriever.py
@@ -1,21 +1,20 @@
 from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
 
+from app.config import QDRANT_URL, QDRANT_COLLECTION_NAME
 from app.rag.embeddings.embedding_model import get_embedding_model
-from app.rag.vectorstore.qdrant_store import COLLECTION_NAME
 
 
 def get_retriever():
-
     embeddings = get_embedding_model()
 
     client = QdrantClient(
-        url="http://localhost:6333"
+        url=QDRANT_URL
     )
 
     vector_store = QdrantVectorStore(
         client=client,
-        collection_name=COLLECTION_NAME,
+        collection_name=QDRANT_COLLECTION_NAME,
         embedding=embeddings,
     )
 
